537c368929e6be684331a96cc0b42622a0d324ae
JuanPaladines <juan@twnel.com>
2022-04-12 11:46:02 -0500

    Dynamic Routes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,14 @@
 const Products = () => {
-    return <h1>Products Page</h1>
-}
+  return (
+    <section>
+      <h1>Products Page</h1>
+      <ul>
+        <li>Book</li>
+        <li>Carpet</li>
+        <li>Online Course</li>
+      </ul>
+    </section>
+  );
+};
 
-export default Products
+export default Products;
